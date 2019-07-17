5d6b5fc0b924a022380edf4d33528ecc5f824518
MahmoudBwedany <Mahmoudbwedany@gmail.com>
2019-07-18 01:22:09 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 /* Your code goes here */
+
+var customerName;
+const leastFavoriteCustomer = "Someone";
+function upperCaseCustomerName(customerName) {
+  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not nob";
+  
+
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = "Something else"
+
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Something else entirely"
+  
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "something";
+  let favoriteCustomer = "Another delcaration"
+
+}
